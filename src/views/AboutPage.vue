8cdba19d403196056cb21f8243057d295a53b4e5
StefanStrezoski <stefan.strezo98@gmail.com>
2026-05-03 22:51:00 +0200

    Changed misstaken copy

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -23,7 +23,7 @@ const currentImage = computed(() => {
         <div class="d-flex align-center">
           <v-icon icon="mdi-alert-circle" color="red-darken-2" size="32" class="mr-2"></v-icon>
           <span class="text-h4 font-weight-black text-red-darken-2" style="letter-spacing: 1px;">{{ t('popup.title')
-          }}</span>
+            }}</span>
         </div>
         <v-btn icon="mdi-close" variant="text" density="compact" color="grey-darken-1"
           @click="showPopup = false"></v-btn>
@@ -34,9 +34,6 @@ const currentImage = computed(() => {
       </div>
     </v-card>
 
-    <div class="text-h4 font-weight-bold mb-6 text-center text-primary-dark">
-      {{ t('programPage.title') }}
-    </div>
     <v-img :src="currentImage" alt="About"
       :class="{ 'desktop-image': currentImage === imgMK || currentImage === imgEN }"></v-img>
   </base-container>
